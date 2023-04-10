7b0f3033ff67190468515e55c889a4ae48e404ff
Siarhei Prakapovich <prokser27@icloud.com>
2023-04-11 02:15:36 +0300

    refactor: add logo

--- conventions/src/App.jsx
+++ conventions/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Deepfake } from './components/Deepfake'
+import Carousel from './components/Carousel';
+
+import Logo_of_the_unUnited_Nations from './assets/Logo_of_the_unUnited_Nations.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="top-dec">
+        <img src={Logo_of_the_unUnited_Nations} className='uun-logo'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Carousel />
+      <Deepfake />
     </div>
   )
 }
--- conventions/src/components/Deepfake.jsx
+++ conventions/src/components/Deepfake.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Conventions from './Database.js'
+import Conventions from '/Frontend/conventions/conventions/src/db/Database'
 
 export function Deepfake() {
-  return (
-    <div>{Object.keys(Conventions.data["Russian"]).map((convention) => {
-        return(
-        <>
-        {Conventions.data["Russian"][convention].map((chapter) => {
-            <h1>{Conventions.data["Russian"][convention][chapter]}</h1>
-            return(
-            <p>{chapter.map((article) => 
-                Object.keys(article).map((content) => article[content]))}</p>)
-        })}
-        </>)
 
-    })}</div>
+  // От оглавления до текста глав - это все компонент Дипфейк
+  // Выбор глав - отдельный компонент, который будет импортироваться (главы в качестве пропсов?)
+
+  const getTreatyText = () => {
+    const treaty = Conventions["Russian"]["Deepfake"];
+    console.log(treaty)
+    return treaty.map((chapter, id) => {
+      return (
+        <>
+          <h1>Chapter {id + 1}</h1>
+          <p>
+            {
+              chapter.map((article) => Object.keys(article).map((content) => article[content]))
+            }
+          </p>
+        </>
+      )
+    })
+  }
+
+  return (
+    <div>
+      {getTreatyText()}
+    </div>
   )
 }
